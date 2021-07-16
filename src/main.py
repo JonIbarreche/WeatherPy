740ed279bb617a8cdc5dd1129c7cafe580650f66
JonIbarreche <55731819+JonIbarreche@users.noreply.github.com>
2021-07-16 22:27:34 +0200

    Updated main.py

--- src/main.py
+++ src/main.py
@@ -1,5 +1,12 @@
 import requests
 from pprint import pprint
 
-Clave_API = ''
+Clave_API = '2562242488218794bece0a82f3e2e7db'
+
 ciudad = input("Introduce una ciudad: ")
+
+url = "http://api.openweathermap.org/data/2.5/weather?q="+Clave_API+"&appid="+ciudad
+
+weather_data = requests.get(url).json()
+
+pprint(weather_data)
